d2fbba95075e4cb8bb825859daf1623c925ca71a
Barthezpro <bartheztonlio@gmail.com>
2025-10-31 14:31:26 +0300

    Application de js:Music player App

--- Music_Player/script.js
+++ Music_Player/script.js
@@ -1,6 +1,16 @@
 var progess=document.getElementById("progress");
-var song=document.getElementById("song");
 var crtlsong=document.getElementById("crtlsong");
+var forward=document.getElementById("forward");
+var backward=document.getElementById("backward");
+var music=document.querySelectorAll(".music");
+var image=document.getElementById("image");
+var cmp=0;
+var table=['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg'];
+var cmpimage=1;
+// console.log(table.length);
+// console.log(table[0]);
+
+var song=music[cmp];
 
 song.onloadedmetadata=function(){/* song.onloadedmetadata = function() { ... } :Cet événement se déclenche lorsque les métadonnées du fichier audio sont chargées.
 Les métadonnées incluent la durée, le type de média, etc.
@@ -23,6 +33,7 @@ function joueLeSon(){
         crtlsong.classList.remove("fa-play");
     }
 }
+
 if (song.play()){
         setInterval(() => {
             progess.value=song.currentTime;
@@ -35,3 +46,29 @@ progess.onchange=function(){
     crtlsong.classList.remove("fa-play");
     crtlsong.classList.add("fa-pause");
 }
+
+// Chanson suivante
+forward.addEventListener('click', function() {
+  song.pause();
+  song.currentTime = 0;
+  cmp = (cmp + 1) % music.length;
+  song = music[cmp];
+  song.play();
+  crtlsong.classList.add("fa-pause");
+  crtlsong.classList.remove("fa-play");
+  image.src=table[cmpimage];
+  cmpimage = (cmpimage + 1) % table.length;
+});
+
+// Chanson précédente
+backward.addEventListener('click', function() {
+  song.pause();
+  song.currentTime = 0;
+  cmp = (cmp - 1 + music.length) % music.length;
+  song = music[cmp];
+  song.play();
+  crtlsong.classList.add("fa-pause");
+  crtlsong.classList.remove("fa-play");
+  image.src=table[cmpimage];
+  cmpimage = (cmpimage - 1 + table.length) % table.length;
+});
