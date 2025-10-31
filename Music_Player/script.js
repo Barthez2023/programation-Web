381b35aad943a97369c9cda9d6f5809ce4be0687
Barthezpro <bartheztonlio@gmail.com>
2025-10-31 17:23:33 +0300

    Application de js:Music player App

--- Music_Player/script.js
+++ Music_Player/script.js
@@ -4,9 +4,12 @@ var forward=document.getElementById("forward");
 var backward=document.getElementById("backward");
 var music=document.querySelectorAll(".music");
 var image=document.getElementById("image");
+var titre=document.getElementById("titre");
+var auteur=document.getElementById("auteur");
 var cmp=0;
 var table=['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg'];
-var cmpimage=1;
+var nom_Music=['Longtemps','Ton pied mon pied','YOLO','Je T\'aime','Merci pour ça'];
+var music_Auteur=['Amir Ft Barthez','Hiro Ft Chidinma','GIMS','Mr_Leo Ft Hiro','Vianney'];
 // console.log(table.length);
 // console.log(table[0]);
 
@@ -49,26 +52,36 @@ progess.onchange=function(){
 
 // Chanson suivante
 forward.addEventListener('click', function() {
-  song.pause();
-  song.currentTime = 0;
-  cmp = (cmp + 1) % music.length;
-  song = music[cmp];
-  song.play();
-  crtlsong.classList.add("fa-pause");
-  crtlsong.classList.remove("fa-play");
-  image.src=table[cmpimage];
-  cmpimage = (cmpimage + 1) % table.length;
+    song.pause();
+    song.currentTime = 0;
+    cmp = (cmp + 1) % music.length;
+    song = music[cmp];
+    song.play();
+    crtlsong.classList.add("fa-pause");
+    crtlsong.classList.remove("fa-play");
+
+    //change∈l'image de la music
+    image.src=table[cmp];
+    //change le nom de la musique
+    titre.innerHTML=nom_Music[cmp];
+    //change le nom de l'auteur de la music
+    auteur.innerHTML=music_Auteur[cmp];
 });
 
 // Chanson précédente
 backward.addEventListener('click', function() {
-  song.pause();
-  song.currentTime = 0;
-  cmp = (cmp - 1 + music.length) % music.length;
-  song = music[cmp];
-  song.play();
-  crtlsong.classList.add("fa-pause");
-  crtlsong.classList.remove("fa-play");
-  image.src=table[cmpimage];
-  cmpimage = (cmpimage - 1 + table.length) % table.length;
+    song.pause();
+    song.currentTime = 0;
+    cmp = (cmp - 1 + music.length) % music.length;
+    song = music[cmp];
+    song.play();
+    crtlsong.classList.add("fa-pause");
+    crtlsong.classList.remove("fa-play");
+
+    //change∈l'image de la music
+    image.src=table[cmp];
+    //change le nom de la musique
+    titre.innerHTML=nom_Music[cmp];
+    //change le nom de l'auteur de la music
+    auteur.innerHTML=music_Auteur[cmp];
 });
