cb28b6bda305419aa426ff8ec1d75b7e4970485f
Barthezpro <bartheztonlio@gmail.com>
2025-12-18 14:01:25 +0300

    Implementation of Drag & Drop

---
+++ drag_drop/script.js
@@ -0,0 +1,27 @@
+var lists=document.getElementsByClassName("list");
+var rightBox=document.getElementById("right");
+var leftBox=document.getElementById("left");
+
+for (const list of lists) {
+    list.addEventListener("dragstart",function(e){
+        let selected=e.target;
+
+        rightBox.addEventListener("dragover",function(e){
+            e.preventDefault();
+            
+        });
+        rightBox.addEventListener("drop" ,function(e){
+            rightBox.appendChild(selected);
+            selected=null;
+        });
+
+        leftBox.addEventListener("dragover",function(e){
+            e.preventDefault();
+            
+        });
+        leftBox.addEventListener("drop" ,function(e){
+            leftBox.appendChild(selected);
+            selected=null;
+        });
+    });
+}
