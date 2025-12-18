9cb536cab91e47cad062bbe0db92c3e01b9649db
Barthezpro <bartheztonlio@gmail.com>
2025-12-18 14:13:30 +0300

    Implementation of Drag & Drop

--- drag_drop/script.js
+++ drag_drop/script.js
@@ -3,14 +3,17 @@ var rightBox=document.getElementById("right");
 var leftBox=document.getElementById("left");
 
 for (const list of lists) {
-    list.addEventListener("dragstart",function(e){
+    list.addEventListener("dragstart",function(e){   
+        //Role de dragstart Se declenche au moment exact ou l'utilisateur commence a glisser un element. Sert a identifier l'element en cours de deplacement
+        //dragstart permet de savoir quel element est en train d'etre glisse
         let selected=e.target;
 
         rightBox.addEventListener("dragover",function(e){
-            e.preventDefault();
-            
+            //Se declenche quand un element est deplace au-dessus d'une zone
+            e.preventDefault();                          //Sans e.preventDefault(), Le navigateur bloque le drop L'evenement drop ne se declenche jamais
+             
         });
-        rightBox.addEventListener("drop" ,function(e){
+        rightBox.addEventListener("drop" ,function(e){                  //Se declenche lorsque l'utilisateur relache la souris
             rightBox.appendChild(selected);
             selected=null;
         });
@@ -25,3 +28,6 @@ for (const list of lists) {
         });
     });
 }
+
+/*Le mecanisme Drag & Drop repose sur l'identification de l'el'ment lors de dragstart, 
+l'autorisation du depot via dragover et l'insertion finale de l'element avec drop.*/
